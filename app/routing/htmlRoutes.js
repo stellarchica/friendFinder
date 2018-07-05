168474afb842fe652a741374336bf89662264d6c
sarah martinez <sarahmartinez@sarahs-MacBook.local>
2018-07-05 13:10:43 -0500

    quotatation fix

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -6,11 +6,11 @@ var path = require("path");
 module.exports = function(app){
     // default GET route that leads to home.html - displays home page
     app.get("/survey", function (req, res) {
-        res.sendFile(path.join(__dirname + '/../public/survey.html'));
+        res.sendFile(path.join(__dirname + "/../public/survey.html"));
     });
 
     // USE route to home page
     app.use(function (req, res) {
-        res.sendFile(path.join(__dirname + '/../public/home.html'));
+        res.sendFile(path.join(__dirname + "/../public/home.html"));
     });
 };
