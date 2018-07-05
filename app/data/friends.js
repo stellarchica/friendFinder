e34552e2a54675209ec8ab48681a4bf46140eba5
sarah martinez <sarahmartinez@sarahs-MacBook.local>
2018-07-05 02:39:57 -0500

    friends file added

---
+++ app/data/friends.js
@@ -0,0 +1,23 @@
+// array that holds all the "friends" that have taken the survey
+
+// initialize starting with "dummy" data
+var friendList = [
+    {
+      name: "Sarah Martinez",
+      profilePic: "https://avatars1.githubusercontent.com/u/37557038?s=400&u=032cac1ea02bf29145ed79a438eab2859f2d19b6&v=4",
+      scores: [
+        "5",
+        "2",
+        "4",
+        "4",
+        "3",
+        "2",
+        "5",
+        "1",
+        "5",
+        "2"
+      ]
+    }
+  ]
+  
+  module.exports = friendList;
